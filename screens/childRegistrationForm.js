81bad131d745b5d69af27575fb5dd1b0e3419e92
ChisengaSol <solmanzambia@gmail.com>
2023-12-19 07:39:35 +0200

    changed immunization fields to checkboxes

--- screens/childRegistrationForm.js
+++ screens/childRegistrationForm.js
@@ -1,74 +1,99 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, Picker } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, CheckBox } from 'react-native';
 
 const ChildRegistrationForm = () => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [age, setAge] = useState('');
   const [gender, setGender] = useState('Male');
-  const [selectedImmunizations, setSelectedImmunizations] = useState([]);
+  const [immunizations, setImmunizations] = useState({
+    BCG: false,
+    MMR: false,
+    RV: false,
+    DTaP: false,
+  });
 
-  const handleRegistration = () => {
-    // Perform form submission or data handling logic here
-    console.log('Submitted Data:', {
-      firstName,
-      lastName,
-      age,
-      gender,
-      selectedImmunizations,
-    });
+  const handleSave = () => {
+    // Handle saving form data here
+    console.log('Saved data:', { firstName, lastName, age, gender, immunizations });
+  };
+
+  const handleCheckboxChange = (immunization) => {
+    setImmunizations({ ...immunizations, [immunization]: !immunizations[immunization] });
   };
 
   return (
-    <View style={{ padding: 20 }}>
+    <View style={styles.container}>
       <Text>First Name:</Text>
       <TextInput
-        style={{ borderWidth: 1, borderColor: 'gray', marginBottom: 10, padding: 5 }}
+        style={styles.input}
         value={firstName}
-        onChangeText={setFirstName}
+        onChangeText={(text) => setFirstName(text)}
       />
-
       <Text>Last Name:</Text>
       <TextInput
-        style={{ borderWidth: 1, borderColor: 'gray', marginBottom: 10, padding: 5 }}
+        style={styles.input}
         value={lastName}
-        onChangeText={setLastName}
+        onChangeText={(text) => setLastName(text)}
       />
-
       <Text>Age:</Text>
       <TextInput
-        style={{ borderWidth: 1, borderColor: 'gray', marginBottom: 10, padding: 5 }}
+        style={styles.input}
         value={age}
-        onChangeText={setAge}
         keyboardType="numeric"
+        onChangeText={(text) => setAge(text)}
       />
-
       <Text>Gender:</Text>
-      <Picker
-        selectedValue={gender}
-        onValueChange={(itemValue) => setGender(itemValue)}
-        style={{ borderWidth: 1, borderColor: 'gray', marginBottom: 10 }}
-      >
-        <Picker.Item label="Male" value="Male" />
-        <Picker.Item label="Female" value="Female" />
-      </Picker>
-
+      <TextInput
+        style={styles.input}
+        value={gender}
+        onChangeText={(text) => setGender(text)}
+      />
       <Text>Immunizations:</Text>
-      <Picker
-        selectedValue={selectedImmunizations}
-        onValueChange={(itemValue) => setSelectedImmunizations(itemValue)}
-        mode="multiple"
-        style={{ borderWidth: 1, borderColor: 'gray', marginBottom: 10 }}
-      >
-        <Picker.Item label="BCG" value="BCG" />
-        <Picker.Item label="MMR" value="MMR" />
-        <Picker.Item label="RV" value="RV" />
-        <Picker.Item label="DTaP" value="DTaP" />
-      </Picker>
-
-      <Button title="Submit" onPress={handleRegistration} />
+      <View style={styles.checkboxContainer}>
+        <CheckBox
+          value={immunizations.BCG}
+          onValueChange={() => handleCheckboxChange('BCG')}
+        />
+        <Text style={styles.checkboxLabel}>BCG</Text>
+        <CheckBox
+          value={immunizations.MMR}
+          onValueChange={() => handleCheckboxChange('MMR')}
+        />
+        <Text style={styles.checkboxLabel}>MMR</Text>
+        <CheckBox
+          value={immunizations.RV}
+          onValueChange={() => handleCheckboxChange('RV')}
+        />
+        <Text style={styles.checkboxLabel}>RV</Text>
+        <CheckBox
+          value={immunizations.DTaP}
+          onValueChange={() => handleCheckboxChange('DTaP')}
+        />
+        <Text style={styles.checkboxLabel}>DTaP</Text>
+      </View>
+      <Button title="Save" onPress={handleSave} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  checkboxLabel: {
+    margin: 8,
+  },
+});
 
 export default ChildRegistrationForm;
