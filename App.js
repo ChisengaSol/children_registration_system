15b696dd82439b21a6459c63cd1f4d637ec93442
ChisengaSol <solmanzambia@gmail.com>
2023-12-19 12:37:00 +0200

    created screen to display registered children

--- App.js
+++ App.js
@@ -3,15 +3,33 @@ import "setimmediate"
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import ChildRegistrationForm from './screens/childRegistrationForm';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import RegisteredChildren from "./screens/registeredChildren";
+
+const Stack = createNativeStackNavigator()
+
+// export default function App() {
+//   return (
+    
+    
+//       <ChildRegistrationForm />
+    
+    
+
+//   );
+// }
+
+
 
 export default function App() {
   return (
-    
-    
-      <ChildRegistrationForm />
-    
-    
-
+    <NavigationContainer>{/* Rest of your app code */}
+      <Stack.Navigator initialRouteName="RegisteredChildren">
+        <Stack.Screen name="ChildRegistrationForm" component={ChildRegistrationForm}/>
+        <Stack.Screen name="RegisteredChildren" component={RegisteredChildren}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/registeredChildren.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button, TouchableOpacity, StyleSheet } from 'react-native';
+import { openDatabase } from 'expo-sqlite';
+
+const  RegisteredChildren = () => {
+    return ( 
+        <View>
+            <Text>Registered kids to appear here</Text>
+        </View>
+     );
+}
+ 
+export default RegisteredChildren;
